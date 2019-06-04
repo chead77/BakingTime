070192d2d572ced19cf437eaf427abe1de11bc44
chead77 <chead77@gmail.com>
2019-06-04 02:29:34 -0500

    Added bottom navigation view to the step detail fragment
    The menu options now change between steps, updating the step instructions and the video being played.

--- app/src/main/java/com/cheadtech/example/bakingtime/fragments/StepDetailFragment.java
+++ app/src/main/java/com/cheadtech/example/bakingtime/fragments/StepDetailFragment.java
@@ -28,13 +28,16 @@ import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class StepDetailFragment extends Fragment {
     private final String tag = getClass().toString();
 
     private Recipe recipe;
-    private Integer currentRecipeStep = 0;
-    private Step currentStep;
+    private Integer currentRecipeStepPosition = 0;
+    private ArrayList<Step> recipeSteps;
 
     private PlayerView playerView;
     private SimpleExoPlayer player;
@@ -42,6 +45,8 @@ public class StepDetailFragment extends Fragment {
     private TextView videoErrorMessageTV;
 
     private TextView stepInstructionsTV;
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,9 +58,17 @@ public class StepDetailFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         Activity activity = getActivity();
-        if (activity == null) {
-            Log.e(tag, "Activity not found");
+        playerView = view.findViewById(R.id.video_player);
+        stepInstructionsTV = view.findViewById(R.id.step_instruction_tv);
+        videoError = view.findViewById(R.id.video_error);
+        videoErrorMessageTV = view.findViewById(R.id.video_error_message_tv);
+        bottomNavigationView = view.findViewById(R.id.step_navigation);
+        if (activity == null || playerView == null || stepInstructionsTV == null || videoError == null
+                || videoErrorMessageTV == null || bottomNavigationView == null) {
+            Log.e(tag, "NULL Activity or View");
             Toast.makeText(requireContext(), getString(R.string.error_please_try_again), Toast.LENGTH_SHORT).show();
+            if (activity != null)
+                activity.finish();
             return;
         }
 
@@ -69,37 +82,17 @@ public class StepDetailFragment extends Fragment {
 
         if (extras.getParcelable(getString(R.string.extra_recipe)) instanceof Recipe)
             recipe = extras.getParcelable(getString(R.string.extra_recipe));
-        currentRecipeStep = extras.getInt(getString(R.string.extra_recipe_step), -1);
-        if (recipe == null || currentRecipeStep == -1) {
-            Log.e(tag, "invalid Recipe or currentRecipeStep");
+        currentRecipeStepPosition = extras.getInt(getString(R.string.extra_recipe_step), -1);
+        if (recipe == null || currentRecipeStepPosition == -1) {
+            Log.e(tag, "Recipe or currentRecipeStepPosition not found in intent Extras");
             Toast.makeText(requireContext(), getString(R.string.error_please_try_again), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        playerView = view.findViewById(R.id.video_player);
-        stepInstructionsTV = view.findViewById(R.id.step_instruction_tv);
-        videoError = view.findViewById(R.id.video_error);
-        videoErrorMessageTV = view.findViewById(R.id.video_error_message_tv);
-        if (playerView == null || stepInstructionsTV == null || videoError == null || videoErrorMessageTV == null) {
-            Log.e(tag, "One or more Views are null");
-            Toast.makeText(requireContext(), getString(R.string.error_please_try_again), Toast.LENGTH_SHORT).show();
-            activity.finish();
-            return;
-        }
-
-        try {
-            currentStep = recipe.steps.get(currentRecipeStep);
-        } catch (IndexOutOfBoundsException e) {
-            Log.e(tag, e.getMessage());
-            Toast.makeText(requireContext(), getString(R.string.error_please_try_again), Toast.LENGTH_SHORT).show();
-            if (getActivity() != null) {
-                getActivity().finish();
-            }
-            return;
-        }
-
-        setupPlayerView();
-        stepInstructionsTV.setText(currentStep.description);
+        recipeSteps = new ArrayList<>(recipe.steps);
+        setupStepNavigation();
+        stepInstructionsTV.setText(recipeSteps.get(currentRecipeStepPosition).description);
+        resetPlayerView();
     }
 
     @Override
@@ -112,29 +105,6 @@ public class StepDetailFragment extends Fragment {
     public void onStop() {
         super.onStop();
         player.release();
-    }
-
-    private void setupPlayerView() {
-        String url = currentStep.videoURL;
-        player = ExoPlayerFactory.newSimpleInstance(requireContext());
-        playerView.setPlayer(player);
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(requireContext(),
-                Util.getUserAgent(requireContext(), getString(R.string.app_name)));
-        Uri uri = Uri.parse(url);
-        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
-        showPlayer();
-        player.setPlayWhenReady(true);
-        player.addListener(new Player.EventListener() {
-            @Override
-            public void onPlayerError(ExoPlaybackException error) {
-                if (currentStep.videoURL.isEmpty())
-                    hidePlayer(getString(R.string.video_not_provided));
-                else
-                    hidePlayer(getString(R.string.video_playback_error));
-            }
-
-        });
-        player.prepare(videoSource);
     }
 
     private void showPlayer() {
@@ -158,5 +128,59 @@ public class StepDetailFragment extends Fragment {
         videoErrorMessageTV.setText(errorMessage);
         videoError.setVisibility(View.VISIBLE);
         playerView.setVisibility(View.GONE);
+    }
+
+    private void resetPlayerView() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(requireContext());
+            playerView.setPlayer(player);
+        } else {
+            player.setPlayWhenReady(false);
+        }
+        String url = recipeSteps.get(currentRecipeStepPosition).videoURL;
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(requireContext(),
+                Util.getUserAgent(requireContext(), getString(R.string.app_name)));
+        Uri uri = Uri.parse(url);
+        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        showPlayer();
+        player.setPlayWhenReady(true);
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+                if (recipeSteps.get(currentRecipeStepPosition).videoURL.isEmpty())
+                    hidePlayer(getString(R.string.video_not_provided));
+                else
+                    hidePlayer(getString(R.string.video_playback_error));
+            }
+        });
+        player.prepare(videoSource);
+    }
+
+    private void setupStepNavigation() {
+        if (bottomNavigationView != null) {
+            bottomNavigationView.getMenu().getItem(0).setEnabled(currentRecipeStepPosition > 0);
+            bottomNavigationView.getMenu().getItem(1).setEnabled(currentRecipeStepPosition < recipeSteps.size() - 1);
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+                if (menuItem.getTitle().equals(getString(R.string.next_step)))
+                    currentRecipeStepPosition++;
+                else if (menuItem.getTitle().equals(getString(R.string.previous_step)))
+                    currentRecipeStepPosition--;
+                else {
+                    Log.e(tag, "Invalid navigation menu option");
+                    Toast.makeText(requireContext(), getString(R.string.error_please_try_again), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+
+                // disable "previous" and "next" nav menu options if the bounds of the step array are reached
+                bottomNavigationView.getMenu().getItem(0).setEnabled(currentRecipeStepPosition > 0);
+                bottomNavigationView.getMenu().getItem(1).setEnabled(currentRecipeStepPosition < recipeSteps.size() - 1);
+
+                stepInstructionsTV.setText(recipeSteps.get(currentRecipeStepPosition).description);
+                resetPlayerView();
+
+                return true;
+            });
+        }
     }
 }
